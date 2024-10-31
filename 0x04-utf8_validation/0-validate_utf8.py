ae5e38045d560804d2678d52e61b97a9a058357b
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-10-31 02:19:59 -0700

    add

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
+
 def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
-    
+
     # Masks to check the number of leading 1's in a byte
     first_byte_mask = 1 << 7
     second_byte_mask = 1 << 6
-    
+
     # Iterate over each integer in the data list
     for num in data:
         # Check only the 8 least significant bits of each integer
         byte = num & 0xFF
-        
+
         if num_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
             if (byte & first_byte_mask) == 0:
@@ -31,9 +33,9 @@ def validUTF8(data):
             # Check for a continuation byte (10xxxxxx)
             if (byte & (first_byte_mask | second_byte_mask)) != second_byte_mask:
                 return False
-            
+
         # Decrement the bytes remaining in the current character
         num_bytes -= 1
-    
+
     # If we finished with no remaining bytes, the encoding is valid
     return num_bytes == 0
