bab10a9b04ddaa1c0d77404b9200a1bf400ec07e
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-11-13 21:54:30 -0800

    looks like i finally Got it

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,23 @@
 #!/usr/bin/node
 
-const https = require('https');
-
-// Helper function to perform an HTTPS GET request and parse JSON
-function fetchJson(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        try {
-          const jsonData = JSON.parse(data);
-          resolve(jsonData);
-        } catch (error) {
-          reject(new Error('Error parsing JSON'));
-        }
-      });
-    }).on('error', (err) => {
-      reject(err);
-    });
-  });
-}
+const axios = require('axios');
 
 // Function to fetch and print characters from the specified movie
 async function getMovieCharacters(movieId) {
   try {
-    const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
-    const filmData = await fetchJson(filmUrl);
+    // Fetch movie details
+    const response = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
+    const filmData = response.data;
 
     console.log(`Characters in '${filmData.title}':\n`);
 
-    // Fetch each character using the new API endpoint
-    const characterPromises = filmData.characters.map((characterUrl) => {
-      // Extract character ID from the URL and use the new API endpoint
-      const characterId = characterUrl.split('/').filter(Boolean).pop();
-      const newCharacterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}`;
-      return fetchJson(newCharacterUrl);
-    });
-
-    // Wait for all character data to be fetched and print names in order
-    const characters = await Promise.all(characterPromises);
-    characters.forEach((character) => {
-      console.log(character.name);
-    });
-
+    // Iterate over character URLs and fetch each character's name
+    for (const characterUrl of filmData.characters) {
+      const characterResponse = await axios.get(characterUrl);
+      console.log(characterResponse.data.name);
+    }
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error fetching data: ${error.message}`);
   }
 }
 
